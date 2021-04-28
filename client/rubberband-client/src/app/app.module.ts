a468623744a53d2c8b111cf2c676560951475b0e
llnorthpolell <marcong0131@gmail.com>
2021-04-28 18:21:37 -0400

    Completed Angular Migration to Angular 11

--- client/rubberband-client/src/app/app.module.ts
+++ client/rubberband-client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+//import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
@@ -14,7 +14,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+//    HttpModule,
     PagesModule,
     SharedModule
   ],
